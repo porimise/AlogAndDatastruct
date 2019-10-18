7631a4eaedceeeaeba22009e90c82e369df4aa3a
allen <846325800@qq.com>
2019-10-18 16:33:42 +0800

    增加linkedlist

---
+++ src/datastruct/list/singlelinkedlist/Linkedelist.java
@@ -0,0 +1,14 @@
+package datastruct.list.singlelinkedlist;
+public class Linkedelist {
+    Node head;
+    class Node{
+       int value;
+        Node next;
+
+        public Node(int value){
+            this.value=value;
+
+        }
+
+    }
+}
